0cf3ecec4fdccf4e544a7bbe8e09a7e99233b342
JonasQuebraScripts018 <gustavohpdelima@gmail.com>
2023-07-28 17:19:04 -0300

    Sex 28 Jul

---
+++ src/main/java/com/MundoSenai/Presenca/Model/M_Pessoa.java
@@ -0,0 +1,75 @@
+package com.MundoSenai.Presenca.Model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "pessoa")
+public class M_Pessoa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private Long cpf;
+    private Long telefone;
+    private String dataNasc;
+    private String senha;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(Long cpf) {
+        this.cpf = cpf;
+    }
+
+    public Long getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(Long telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getDataNasc() {
+        return dataNasc;
+    }
+
+    public void setDataNasc(String dataNasc) {
+        this.dataNasc = dataNasc;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+
+}
